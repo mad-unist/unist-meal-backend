a52c4c082338eb101ebd9aeb136c727dc8d2c911
jspark98 <jspark98@unist.ac.kr>
2022-05-19 16:24:44 +0900

    chore: re setting for restaurant

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
     place = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
-    number = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
     content = models.CharField(max_length=100)
