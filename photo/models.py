03105722f60d9c8c3f5b15f690192df39f9af360
jspark98 <jspark98@unist.ac.kr>
2022-05-19 16:19:31 +0900

    chore: increase photo url length

--- photo/models.py
+++ photo/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Photo(models.Model):
     name = models.CharField(max_length=50)
-    url = models.CharField(max_length=100)
+    url = models.CharField(max_length=500)
