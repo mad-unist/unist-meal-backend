3a2380a42e066c556df131ad99650bd8e8028160
jongseo98 <jspark98@unist.ac.kr>
2022-08-02 00:25:52 +0900

    chore: secret key 원상태

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,8 @@ def get_secret(setting):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret("SECRET_KEY")
+# SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '(django-insecure-12s0%yu0gdqg5*o$#&#nx!r3q(gx&l9%lwz!m7pndcwzx&kufz)w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
