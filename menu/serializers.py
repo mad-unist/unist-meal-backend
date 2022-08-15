3a7b1bd7fece431f8128f4f000d9b838205cac40
jongseo98 <jspark98@unist.ac.kr>
2022-08-15 17:49:34 +0900

    feat: add rating for menu API

--- menu/serializers.py
+++ menu/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Menu
+from menu.models import Menu
+from django.db.models import Avg
 
 
 class MenuSerializer(serializers.ModelSerializer):
+    rating = serializers.SerializerMethodField()
     class Meta:
         model = Menu
-        fields = "__all__"
+        fields = ("id", "place", "type", "time", "content", "calorie", "date", "rating")
+        
+    def get_rating(self, obj):
+        rating = obj.ratings.all().aggregate(avg_rating=Avg("rating"))["avg_rating"]
+        return rating if rating else 0.0
