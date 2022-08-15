f15496ea457835764366314b0e99b766075a047f
jongseo98 <jspark98@unist.ac.kr>
2022-08-15 23:31:39 +0900

    feat: make user id to string

--- account/models.py
+++ account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    id = models.IntegerField(unique=True, primary_key=True)
+    id = models.CharField(unique=True, primary_key=True, max_length=255)
     email = models.EmailField(unique=True, null=True, blank=True)
     
     def __str__(self) -> str:
--- rating/urls.py
+++ rating/urls.py
@@ -5,5 +5,5 @@ app_name="rating"
 
 urlpatterns = [
     path("v1/ratings", views.RatingList.as_view(), name="v1_rating_list"),
-    path("v1/ratings/<int:user_id>", views.RatingListByUser.as_view(), name="v1_rating_list_by_user"),
+    path("v1/ratings/<str:user_id>", views.RatingListByUser.as_view(), name="v1_rating_list_by_user"),
 ]
