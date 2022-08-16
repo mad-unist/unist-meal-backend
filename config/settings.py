04adcf23e5b613e82882fc8741a53376f557f35e
jongseo98 <jspark98@unist.ac.kr>
2022-08-16 22:48:43 +0900

    feat: make env var

--- config/settings.py
+++ config/settings.py
@@ -11,41 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os, json
+import os
 import dj_database_url
-from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# secret_file = os.path.join(BASE_DIR, 'secrets.json')
-
-# with open(secret_file) as f:
-#     secrets = json.loads(f.read())
-
-# def get_secret(setting):
-#     """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
-#     try:
-#         return secrets[setting]
-#     except KeyError:
-#         error_msg = "Set the {} environment variable".format(setting)
-#         raise ImproperlyConfigured(error_msg)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = get_secret("SECRET_KEY")
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "SECRET_KEY",
-)
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
-
-print(SECRET_KEY)
-print(DEBUG)
 
 ALLOWED_HOSTS = ["*"]
 
