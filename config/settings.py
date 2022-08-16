dca94877aa6d6bdc9b80fd6580616cd738f09ddb
jongseo98 <jspark98@unist.ac.kr>
2022-08-16 22:52:47 +0900

    chore: set ALLOW HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["unist-meal-backend.herokuapp.com", "localhost"]
 
 
 # Application definition
