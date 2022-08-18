6472b25a5ce48d1ec36d9776bb2c61d5babb5ac8
jongseo98 <jspark98@unist.ac.kr>
2022-08-18 21:42:39 +0900

    feat: rating count for menu

--- menu/serializers.py
+++ menu/serializers.py
@@ -5,10 +5,15 @@ from django.db.models import Avg
 
 class MenuSerializer(serializers.ModelSerializer):
     rating = serializers.SerializerMethodField()
+    rating_count = serializers.SerializerMethodField()
     class Meta:
         model = Menu
-        fields = ("id", "place", "type", "time", "content", "calorie", "date", "rating")
+        fields = ("id", "place", "type", "time", "content", "calorie", "date", "rating", "rating_count")
         
     def get_rating(self, obj):
         rating = obj.ratings.all().aggregate(avg_rating=Avg("rating"))["avg_rating"]
         return rating if rating else 0.0
+    
+    def get_rating_count(self, obj):
+        return obj.ratings.all().count()
+    
