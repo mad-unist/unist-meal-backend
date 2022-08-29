9bbbaf049c3d294e6e4765d8e5abb71ab072d066
jongseo98 <jspark98@unist.ac.kr>
2022-08-29 18:40:30 +0900

    feat: change url for deleting user api

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,5 @@ from account import views
 
 urlpatterns = [
     path("v1/users", views.UserList.as_view(), name="v1_user_list"),
-    path("v1/users/<int:id>", views.UserDetail.as_view(), name="v1_user_detail"),
+    path("v1/user/<str:id>", views.UserDetail.as_view(), name="v1_user_detail"),
 ]
