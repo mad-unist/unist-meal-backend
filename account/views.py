02f00ff2dab8c0b87b6d6b5828f9a1bb0d1a08e7
jongseo98 <jspark98@unist.ac.kr>
2022-08-30 16:24:36 +0900

    fix: change delete user for post method

--- account/views.py
+++ account/views.py
@@ -1,13 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from .models import User
 from account.serializers import UserSerializer
-from account.services import delete_user, fetch_users, create_user, get_user
+import account.services as services
 
 class UserList(APIView):
     
     def get(self, request):
-        users = fetch_users()
+        users = services.fetch_users()
         data = UserSerializer(users, many=True).data
         return Response({"data": data}, status=200)
     
@@ -15,21 +16,19 @@ class UserList(APIView):
         id = request.data["user_id"]
         email = request.data["email"] if "email" in request.data else None
         if User.objects.filter(id=id).exists():
-            user = get_user(id)
+            user = services.get_user(id)
         else:
-            user = create_user(id, email)
+            user = services.create_user(id, email)
         data = UserSerializer(user).data
         return Response({"data": data}, status=200)
-
-class UserDetail(APIView):
-    
-    def get(self, request, id):
-        id = request.data["user_id"]
-        user = get_user(id)
-        data = UserSerializer(user).data
-        return Response({"data": data}, status=200)
-    
+        
     def delete(self, request):
         id = request.data["user_id"]
         delete_user(id)
-        return Response({"message": "success"}, status=204)
+        return Response({"data": "success"}, status=204)
+
+@api_view(["POST"])
+def delete_user(request):
+    id = request.data["user_id"]
+    services.delete_user(id)
+    return Response({"data": "success"}, status=200)
