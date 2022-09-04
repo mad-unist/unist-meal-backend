55396f44e5125f555b394cbfcc6d52dc876a5203
jongseo98 <jspark98@unist.ac.kr>
2022-09-04 22:08:31 +0900

    feat: make API for uploading student menu file

--- util/views.py
+++ util/views.py
@@ -44,7 +44,26 @@ class ProcessExcelMenuDormitory(APIView):
         return Response({"count": len(datas), "data": MenuSerializer(datas, many=True).data}, status=200)
     
 class ProcessExcelMenuStudent(APIView):
-    pass
+    permission_classes = [IsAdminUser]
+    def process_menu(self, worksheet, place):
+        day_idx = ["E", "G", "I", "K", "M"]
+        date_idx = 5
+        week_lunch_idx = [6, 13, "점심"]
+        evening_idx = [17, 24, "저녁"]
+        
+        datas = []
+        date = worksheet["E"][date_idx].value
+        dates= make_7dates(date)
+        for i in range(5):  
+            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], week_lunch_idx[0], week_lunch_idx[1], week_lunch_idx[2])) # 점심
+            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], evening_idx[0], evening_idx[1], evening_idx[2])) # 저녁
+        datas = list(filter(lambda item: item is not None, datas))
+        return datas
+        
+    def post(self, requset):
+        worksheet = get_excel_sheet(self.request.FILES["excel_file"])
+        datas = self.process_menu(worksheet, places[1])        
+        return Response({"count": len(datas), "data": MenuSerializer(datas, many=True).data}, status=200)
     
     
 class ProcessExcelMenuProfessor(APIView):
