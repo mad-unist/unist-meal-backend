92cdc74b73bd8a24322a9765bc1378168985a3e6
jongseo98 <jspark98@unist.ac.kr>
2022-09-23 15:50:14 +0900

    식단표 업로드 트랜잭션 적용 및 칼로리 인트형으로 파싱

--- util/services.py
+++ util/services.py
@@ -14,8 +14,7 @@ def make_menu(
     content_start, content_end,
     time, type=""):
     calorie = worksheet[day][content_end].value
-    if calorie is None:
-        return None
+    calorie = parse_calorie(calorie)
     contents = ""
     for cell in worksheet[day][content_start:content_end]:
         if cell.value is not None and cell.value != "★":
@@ -39,4 +38,16 @@ def make_7dates(day):
     for i in range(1, 7):
         dates.append(day + timedelta(days=i))
     return dates
+
+def parse_calorie(calorie):
+    if calorie is None:
+        return None
+    if isinstance(calorie, int):
+        return calorie
+    if calorie.find(",") != -1:
+        calorie = calorie.replace(",", "")
+    if calorie.find("Kcal") != -1:
+        calorie = calorie.split("Kcal")[0]
+    print("calorie", calorie)
+    return int(calorie)
     
--- util/views.py
+++ util/views.py
@@ -1,9 +1,12 @@
+from xml.dom import ValidationErr
 from django.shortcuts import render
 from menu.serializers import MenuSerializer
 from util.services import get_excel_sheet, make_7dates, make_menu
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
+
+from django.db import transaction
 
 
 places = ["기숙사식당", "학생식당", "교직원식당"]
@@ -13,30 +16,33 @@ class UploadFile(APIView):
     def get(self, requset):
         return render(self.request, 'util/index.html')
 
+
 class ProcessExcelMenuDormitory(APIView):
     permission_classes = [IsAdminUser]
-    
+
+    @transaction.atomic(using='default')
     def process_menu(self, worksheet, place):
-        day_idx = ["E", "G", "I", "K", "M", "O", "Q"]
-        date_idx = 6
-        moring_idx = [7, 13, "아침"]
-        week_lunch_idx = [[14, 21, "점심", "한식"], [22, 28, "점심", "할랄"]]
-        # weekend_lunch_idx = [14, 27, "점심"]
-        evening_idx = [[35, 42, "저녁"], [43, 49, "저녁", "할랄"]]
-        
-        datas = []
-        date = worksheet["E"][date_idx].value
-        dates= make_7dates(date)
-        for i in range(7):  
-            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], moring_idx[0], moring_idx[1], moring_idx[2])) # 아침
-            # if day_idx[i] in ["O", "Q"]: # 주말
-            #     datas.append(make_menu(worksheet, place, day_idx[i], dates[i],week_lunch_idx[0], week_lunch_idx[1], week_lunch_idx[2])) # 점심 (한식)
-            # else:
-            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], week_lunch_idx[0][0], week_lunch_idx[0][1], week_lunch_idx[0][2], week_lunch_idx[0][3])) # 점심 (한식)
-            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], week_lunch_idx[1][0], week_lunch_idx[1][1], week_lunch_idx[1][2], week_lunch_idx[1][3])) # 점심 (할랄)
-            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], evening_idx[0][0], evening_idx[0][1], evening_idx[0][2])) # 저녁
-            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], evening_idx[1][0], evening_idx[1][1], evening_idx[1][2], evening_idx[1][3])) # 저녁 (할랄)
-        datas = list(filter(lambda item: item is not None, datas))
+        try:
+            with transaction.atomic():
+                day_idx = ["E", "G", "I", "K", "M", "O", "Q"]
+                date_idx = 6
+                moring_idx = [7, 13, "아침"]
+                week_lunch_idx = [[14, 21, "점심", "한식"], [22, 28, "점심", "할랄"]]
+                evening_idx = [[35, 42, "저녁"], [43, 49, "저녁", "할랄"]]
+                
+                datas = []
+                date = worksheet["E"][date_idx].value
+                dates= make_7dates(date)
+                for i in range(7):  
+                    datas.append(make_menu(worksheet, place, day_idx[i], dates[i], moring_idx[0], moring_idx[1], moring_idx[2])) # 아침
+                    datas.append(make_menu(worksheet, place, day_idx[i], dates[i], week_lunch_idx[0][0], week_lunch_idx[0][1], week_lunch_idx[0][2], week_lunch_idx[0][3])) # 점심 (한식)
+                    datas.append(make_menu(worksheet, place, day_idx[i], dates[i], week_lunch_idx[1][0], week_lunch_idx[1][1], week_lunch_idx[1][2], week_lunch_idx[1][3])) # 점심 (할랄)
+                    datas.append(make_menu(worksheet, place, day_idx[i], dates[i], evening_idx[0][0], evening_idx[0][1], evening_idx[0][2])) # 저녁
+                    datas.append(make_menu(worksheet, place, day_idx[i], dates[i], evening_idx[1][0], evening_idx[1][1], evening_idx[1][2], evening_idx[1][3])) # 저녁 (할랄)
+                datas = list(filter(lambda item: item is not None, datas))
+        except:
+            raise ValidationErr("식단표 업로드에 실패했습니다.")
+
         return datas
         
     def post(self, requset):
@@ -46,21 +52,27 @@ class ProcessExcelMenuDormitory(APIView):
     
 class ProcessExcelMenuStudent(APIView):
     permission_classes = [IsAdminUser]
+
+    @transaction.atomic(using='default')
     def process_menu(self, worksheet, place):
-        day_idx = ["E", "G", "I", "K", "M"]
-        date_idx = 5
-        week_lunch_idx = [6, 13, "점심"]
-        evening_idx = [17, 24, "저녁"]
-        
-        datas = []
-        date = worksheet["E"][date_idx].value
-        dates= make_7dates(date)
-        for i in range(5):  
-            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], week_lunch_idx[0], week_lunch_idx[1], week_lunch_idx[2])) # 점심
-            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], evening_idx[0], evening_idx[1], evening_idx[2])) # 저녁
-        datas = list(filter(lambda item: item is not None, datas))
+        try:
+            with transaction.atomic():
+                day_idx = ["E", "G", "I", "K", "M"]
+                date_idx = 5
+                week_lunch_idx = [6, 12, "점심"]
+                evening_idx = [16, 22, "저녁"]
+                
+                datas = []
+                date = worksheet["E"][date_idx].value
+                dates= make_7dates(date)
+                for i in range(5):  
+                    datas.append(make_menu(worksheet, place, day_idx[i], dates[i], week_lunch_idx[0], week_lunch_idx[1], week_lunch_idx[2])) # 점심
+                    datas.append(make_menu(worksheet, place, day_idx[i], dates[i], evening_idx[0], evening_idx[1], evening_idx[2])) # 저녁
+                datas = list(filter(lambda item: item is not None, datas))
+        except:
+            raise ValidationErr("식단표 업로드에 실패했습니다.")
         return datas
-        
+    
     def post(self, requset):
         worksheet = get_excel_sheet(self.request.FILES["excel_file"])
         datas = self.process_menu(worksheet, places[1])        
@@ -69,19 +81,25 @@ class ProcessExcelMenuStudent(APIView):
     
 class ProcessExcelMenuProfessor(APIView):
     permission_classes = [IsAdminUser]
+
+    @transaction.atomic(using='default')
     def process_menu(self, worksheet, place):
-        day_idx = ["E", "G", "I", "K", "M"]
-        date_idx = 5
-        week_lunch_idx = [6, 14, "점심"]
-        evening_idx = [19, 26, "저녁"]
-        
-        datas = []
-        date = worksheet["E"][date_idx].value
-        dates= make_7dates(date)
-        for i in range(5):  
-            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], week_lunch_idx[0], week_lunch_idx[1], week_lunch_idx[2])) # 점심
-            datas.append(make_menu(worksheet, place, day_idx[i], dates[i], evening_idx[0], evening_idx[1], evening_idx[2])) # 저녁
-        datas = list(filter(lambda item: item is not None, datas))
+        try:
+            with transaction.atomic():
+                day_idx = ["E", "G", "I", "K", "M"]
+                date_idx = 5
+                week_lunch_idx = [6, 14, "점심"]
+                evening_idx = [19, 26, "저녁"]
+                
+                datas = []
+                date = worksheet["E"][date_idx].value
+                dates= make_7dates(date)
+                for i in range(5):  
+                    datas.append(make_menu(worksheet, place, day_idx[i], dates[i], week_lunch_idx[0], week_lunch_idx[1], week_lunch_idx[2])) # 점심
+                    datas.append(make_menu(worksheet, place, day_idx[i], dates[i], evening_idx[0], evening_idx[1], evening_idx[2])) # 저녁
+                datas = list(filter(lambda item: item is not None, datas))
+        except:
+            raise ValidationErr("식단표 업로드에 실패했습니다.")
         return datas
         
     def post(self, requset):
