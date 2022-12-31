6db97fdebf2aebc6ba71532618259143cc625e68
박종서 <38155105+jongseo98@users.noreply.github.com>
2022-12-31 14:57:43 +0900

    feat: add vacation for dorm in excel (#10)

--- util/urls.py
+++ util/urls.py
@@ -6,6 +6,7 @@ app_name="util"
 urlpatterns = [
     path("v1/upload-file", views.UploadFile.as_view(), name="v1_upload_file"),
     path("v1/process-menus-dormitory", views.ProcessExcelMenuDormitory.as_view(), name="v1_process_menus_dormitory"),
+    path("v1/process-menus-dormitory-vacation", views.ProcessExcelMenuDormitoryVacation.as_view(), name="v1_process_menus_dormitory_vacation"),
     path("v1/process-menus-student", views.ProcessExcelMenuStudent.as_view(), name="v1_process_menus_student"),
     path("v1/process-menus-professor", views.ProcessExcelMenuProfessor.as_view(), name="v1_process_menus_professor"),
 ]
