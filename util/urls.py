6fafa9af357eb0bb39d5dc47ff85d584b6d28ae3
jongseo98 <jspark98@unist.ac.kr>
2023-03-23 18:42:11 +0900

    기숙사 식당 V3 엑셀

--- util/urls.py
+++ util/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("v1/upload-file", views.UploadFile.as_view(), name="v1_upload_file"),
     path("v1/process-menus-dormitory", views.ProcessExcelMenuDormitory.as_view(), name="v1_process_menus_dormitory"),
     path("v1/process-menus-dormitory2", views.ProcessExcelMenuDormitory2.as_view(), name="v1_process_menus_dormitory2"),
+    path("v1/process-menus-dormitory3", views.ProcessExcelMenuDormitory3.as_view(), name="v1_process_menus_dormitory3"),
     path("v1/process-menus-dormitory-vacation", views.ProcessExcelMenuDormitoryVacation.as_view(), name="v1_process_menus_dormitory_vacation"),
     
     path("v1/process-menus-student", views.ProcessExcelMenuStudent.as_view(), name="v1_process_menus_student"),
